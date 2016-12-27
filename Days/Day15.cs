68e2f04d771808ec6a2b4a499a9b38c40d95ef2f
Danny <dannykoppel@gmail.com>
2016-12-27 12:53:23 +0100

    Day 15 + 16
    Lunch break well spend  :)

---
+++ Days/Day15.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2016.Days
+{
+    internal sealed class Day15 : IDay
+    {
+        private readonly IList<string> _input = File.ReadAllLines($"Resources/InputDay15.txt").ToList();
+
+        public void Part1()
+        {
+            var discs = Parse().ToList();
+            Console.WriteLine($"Part1: t: {Solve(discs)}");
+        }
+
+        public void Part2()
+        {
+            _input.Add("Disc #7 has 11 positions; at time=0, it is at position 0.");
+            var discs = Parse().ToList();
+            Console.WriteLine($"Part2: t: {Solve(discs)}");
+        }
+
+        private static int Solve(IList<Disc> discs)
+        {
+            return Enumerable.Range(1, int.MaxValue).First(t => discs.All(d => d.IsOpen(t)));
+        }
+
+        private IEnumerable<Disc> Parse()
+        {
+            const string regexpr = @"(Disc \#)(\d*)\s*(has)\s*(\d*)\s*(positions; at time=0, it is at position)\s*(\d*)(\.)";
+            foreach (string line in _input)
+            {
+                Match match = Regex.Match(line, regexpr);
+                yield return new Disc(int.Parse(match.Groups[2].Value), 
+                                      int.Parse(match.Groups[4].Value), 
+                                      int.Parse(match.Groups[6].Value));
+            }                    
+        }
+
+    }
+
+    internal sealed class Disc
+    {
+        private readonly int _number;
+        private readonly int _positions;
+        private readonly int _startPosition;
+
+        public Disc(int number, int position, int startPosition)
+        {
+            _number = number;
+            _positions = position;
+            _startPosition = startPosition;
+        }
+
+        public bool IsOpen(int t)
+        {
+            return (_startPosition + _number + t) % _positions == 0;
+        }
+    }
+}
---
+++ Days/Day16.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2016.Days
+{
+    internal sealed class Day16 : IDay
+    {
+        private const string Input = "01110110101001000";
+        public void Part1()
+        {            
+            Console.WriteLine($"Part1 - Checksum: {Solve(272)}");
+        }
+
+        public void Part2()
+        {
+            Console.WriteLine($"Part2 - Checksum: {Solve(35651584)}");
+        }
+
+        private string Solve(int length)
+        {
+            string processing = Input;
+            while (processing.Length < length)
+            {
+                processing = Generate(processing);
+            }
+            return Checksum(string.Join("", processing.Take(length)));
+        }
+
+        private static string Generate(string value)
+        {
+            string a = value;
+            string b = string.Join("", a.Reverse().ToArray());
+            b = string.Join("", b.Select(c => c == '1' ? '0' : '1'));
+            return $"{a}0{b}";
+        }
+
+        private static string Checksum(string value)
+        {
+            StringBuilder checksum = new StringBuilder();
+            for (int i = 0; i < value.Length - 1; i += 2)
+            {
+                if (value[i] == value[i + 1])
+                    checksum.Append("1");
+                else
+                    checksum.Append("0");
+            }
+
+            if(checksum.Length % 2 == 0)
+                return Checksum(checksum.ToString());
+            return checksum.ToString();
+        }
+    }
+}
